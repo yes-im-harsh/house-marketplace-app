2f3dcdfd07cc14b40f22c8f16154d9178ae8ebe1
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-07-19 13:56:35 +0530

    Start Create Listing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { getAuth, updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
+
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -93,6 +96,12 @@ const Profile = () => {
               />
             </form>
           </div>
+
+          <Link to="/create-listing" className="createListing">
+            <img src={homeIcon} alt="home-icon" />
+            <p>Sell or rent your home</p>
+            <img src={arrowRight} alt="arrow-right" />
+          </Link>
         </main>
       </div>
     </>
