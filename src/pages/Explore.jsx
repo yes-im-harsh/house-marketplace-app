b4c4736e2da026f42e090e8aa71d06f46f070761
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-07-24 23:06:28 +0530

    Explore Slider

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import Slider from "../components/Slider";
 
 const Explore = () => {
   return (
@@ -10,7 +11,7 @@ const Explore = () => {
       </header>
 
       <main>
-        {/* Slider */}
+        <Slider />
 
         <p className="exploreCategoryHeading">Categories</p>
 
